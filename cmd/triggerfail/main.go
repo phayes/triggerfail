6523ecbb9a1e0209a7de5a4687caced844367437
phayes <patrick.d.hayes@gmail.com>
2014-10-31 14:38:45 -0700

    Splitting out a library

---
+++ cmd/triggerfail/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/phayes/triggerfail/triggerfail"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+	"syscall"
+)
+
+var (
+	OptTrigger string
+	OptAbort   bool
+	OptVerbose bool
+	Triggers   []string
+	Failed     bool
+)
+
+func main() {
+	flag.StringVar(&OptTrigger, "match", "", "Space seperate strings to match. If a match is found the exit code will be 0")
+	flag.BoolVar(&OptAbort, "abort", false, "Abort a running command if a match is found. If abort is not passed the command is allowed to run to completion")
+	flag.BoolVar(&OptVerbose, "v", false, "Verbose. Print the reason why we failed the command.")
+	flag.Parse()
+
+	// Parse the triggers
+	Triggers = strings.Split(OptTrigger, " ")
+
+	args := flag.Args()
+
+	if len(args) == 0 {
+		fmt.Println("triggerfail let's you fail a program with an exit status of 1 if a string appears in it's output (either stderr or stdout). Use `checkfail --help` to see a list of available options.")
+		os.Exit(0)
+	}
+
+	root := args[0]
+	rest := args[1:]
+	cmd := exec.Command(root, rest...)
+
+	found, err := triggerfail.RunCommand(cmd, Triggers, os.Stdout, os.Stderr, OptAbort)
+	if OptVerbose && len(found) != 0 {
+		for _, trig := range found {
+			fmt.Println("Found trigger " + trig)
+		}
+	}
+	if err != nil {
+		if exiterr, ok := err.(*exec.ExitError); ok {
+			// The program has exited with an exit code != 0
+			if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+				os.Exit(status.ExitStatus())
+			} else {
+				// Unknown non-zero exit-status, exit with status 1
+				os.Exit(1)
+			}
+		} else {
+			// triggerfail failed internally for some reason
+			log.Fatal(err.Error())
+		}
+	}
+	if len(found) == 0 {
+		os.Exit(0)
+	} else {
+		os.Exit(1)
+	}
+}
--- triggerfail/triggerfail.go
+++ triggerfail/triggerfail.go
@@ -1,70 +1,10 @@
-package main
+package triggerfail
 
 import (
-	"flag"
-	"fmt"
 	"io"
-	"log"
-	"os"
 	"os/exec"
 	"strings"
-	"syscall"
 )
-
-var (
-	OptTrigger string
-	OptAbort   bool
-	OptVerbose bool
-	Triggers   []string
-	Failed     bool
-)
-
-func main() {
-	flag.StringVar(&OptTrigger, "match", "", "Space seperate strings to match. If a match is found the exit code will be 0")
-	flag.BoolVar(&OptAbort, "abort", false, "Abort a running command if a match is found. If abort is not passed the command is allowed to run to completion")
-	flag.BoolVar(&OptVerbose, "v", false, "Verbose. Print the reason why we failed the command.")
-	flag.Parse()
-
-	// Parse the triggers
-	Triggers = strings.Split(OptTrigger, " ")
-
-	args := flag.Args()
-
-	if len(args) == 0 {
-		fmt.Println("triggerfail let's you fail a program with an exit status of 1 if a string appears in it's output (either stderr or stdout). Use `checkfail --help` to see a list of available options.")
-		os.Exit(0)
-	}
-
-	root := args[0]
-	rest := args[1:]
-	cmd := exec.Command(root, rest...)
-
-	found, err := RunCommand(cmd, Triggers, os.Stdout, os.Stderr, OptAbort)
-	if OptVerbose && len(found) != 0 {
-		for _, trig := range found {
-			fmt.Println("Found trigger " + trig)
-		}
-	}
-	if err != nil {
-		if exiterr, ok := err.(*exec.ExitError); ok {
-			// The program has exited with an exit code != 0
-			if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
-				os.Exit(status.ExitStatus())
-			} else {
-				// Unknown non-zero exit-status, exit with status 1
-				os.Exit(1)
-			}
-		} else {
-			// triggerfail failed internally for some reason
-			log.Fatal(err.Error())
-		}
-	}
-	if len(found) == 0 {
-		os.Exit(0)
-	} else {
-		os.Exit(1)
-	}
-}
 
 // Run a command, checking if triggers were found in it's output
 // It returns a list of triggers found in the stdout and stderr of the running command
@@ -124,9 +64,9 @@ func RunCommand(cmd *exec.Cmd, triggers []string, stdout io.Writer, stderr io.Wr
 		stderr.Write(stderrbuff[:n])
 
 		// If stdout contains a trigger, log it and possibly abort
-		for _, Trigger := range Triggers {
-			if strings.Contains(string(stdoutbuff), Trigger) {
-				found = append(found, Trigger) //@@TODO: mutex
+		for _, trigger := range triggers {
+			if strings.Contains(string(stdoutbuff), trigger) {
+				found = append(found, trigger) //@@TODO: mutex
 				if abort {
 					if !done {
 						cmd.Process.Kill() // abort the running command
@@ -137,9 +77,9 @@ func RunCommand(cmd *exec.Cmd, triggers []string, stdout io.Writer, stderr io.Wr
 		}
 
 		// If stderr contains a trigger, log it and possibly abort
-		for _, Trigger := range Triggers {
-			if strings.Contains(string(stderrbuff), Trigger) {
-				found = append(found, Trigger) //@@TODO: mutex
+		for _, trigger := range triggers {
+			if strings.Contains(string(stderrbuff), trigger) {
+				found = append(found, trigger) //@@TODO: mutex
 				if abort {
 					if !done {
 						cmd.Process.Kill() // abort the running command
