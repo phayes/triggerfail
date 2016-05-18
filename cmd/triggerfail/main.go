dbe9e3ce3705e31f97ba8813044a0c12c76f167d
phayes <patrick.d.hayes@gmail.com>
2016-05-18 16:05:35 -0700

    Updating command to use new API. Adding --stderr and --stdout options

--- cmd/triggerfail/main.go
+++ cmd/triggerfail/main.go
@@ -3,26 +3,31 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/phayes/triggerfail/triggerfail"
 	"log"
 	"os"
 	"os/exec"
 	"strings"
 	"syscall"
+
+	"github.com/phayes/triggerfail/triggerfail"
 )
 
 var (
 	OptTrigger string
 	OptAbort   bool
 	OptVerbose bool
+	OptStdErr  bool
+	OptStdOut  bool
 	Triggers   []string
 	Failed     bool
 )
 
 func Usage() {
-	fmt.Println("triggerfail - fail a command with an exit status of 1 if a string appears in it's output (either stderr or stdout)\n")
+	fmt.Println("triggerfail - fail a command with an exit status of 1 if a string appears in it's output (either stderr or stdout)")
+	fmt.Println("")
 	fmt.Println("USAGE")
-	fmt.Println("  triggerfail \"<space-seperated-strings>\" [--abort] [-v] <command>\n")
+	fmt.Println("  triggerfail \"<space-seperated-strings>\" [--abort] [-v] <command>")
+	fmt.Println("")
 	fmt.Println("OPTIONS")
 	flag.PrintDefaults()
 	fmt.Println("\nEXAMPLE")
@@ -33,6 +38,9 @@ func Usage() {
 func main() {
 	flag.BoolVar(&OptAbort, "abort", false, "Abort a running command if a match is found. If abort is not passed the command is allowed to run to completion")
 	flag.BoolVar(&OptVerbose, "v", false, "Verbose. Print the reason why we failed the command.")
+	flag.BoolVar(&OptStdErr, "stderr", false, "Only examine stderr for triggers.")
+	flag.BoolVar(&OptStdOut, "stdout", false, "Only examine stdout for triggers.")
+
 	flag.Usage = Usage
 	flag.Parse()
 
@@ -41,6 +49,12 @@ func main() {
 	if len(args) <= 1 {
 		Usage()
 		os.Exit(0)
+	}
+
+	// Sanity check
+	if OptStdOut && OptStdErr {
+		fmt.Println("Cannot set both --stderr and --stdout at the same time.")
+		os.Exit(1)
 	}
 
 	// Parse the triggers
@@ -52,7 +66,15 @@ func main() {
 	cmd := exec.Command(root, rest...)
 	cmd.Stdin = os.Stdin
 
-	found, err := triggerfail.RunCommand(cmd, Triggers, os.Stdout, os.Stderr, OptAbort)
+	opts := triggerfail.Options{
+		Abort:        OptAbort,
+		Stdout:       os.Stdout,
+		Stderr:       os.Stderr,
+		IgnoreStdErr: OptStdOut,
+		IgnoreStdOut: OptStdErr,
+	}
+
+	found, err := triggerfail.RunCommand(cmd, Triggers, opts)
 	if OptVerbose && len(found) != 0 {
 		for _, trig := range found {
 			fmt.Println("Found trigger " + trig)
